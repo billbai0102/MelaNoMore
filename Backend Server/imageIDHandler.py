e45cc976db288e85344530ca8d089cef5a6b84b5
andreystarenky <47681471+andreystarenky@users.noreply.github.com>
2020-07-19 14:13:17 -0400

    imageIDHandler image get done

--- Backend Server/imageIDHandler.py
+++ Backend Server/imageIDHandler.py
@@ -1,4 +1,31 @@
+import requests
+from PIL import Image
 
 # Uses the imageID to get the image from Cloudinary API, and feeds it to the model
 def handleImageID(imageID):
-    print(imageID)
+    url = "https://res.cloudinary.com/starenkysoftware/image/upload/v1595173473/charterhacks/"+ imageID +".jpg"
+    response = requests.get(url)
+
+    imageData = response.content
+
+    imageFile = open("imageReceived.png", 'wb')
+    imageFile.write(imageData)
+    imageFile.close()
+
+    image = Image.open("imageReceived.png")
+    image.show()
+
+def getURL():
+    url = "https://res.cloudinary.com/starenkysoftware/image/upload/v1595173473/charterhacks/charter_hacks_image.jpg"
+    response = requests.get(url)
+
+    imageData = response.content
+
+    imageFile = open("imageReceived.png", 'wb')
+    imageFile.write(imageData)
+    imageFile.close()
+
+    image = Image.open("imageReceived.png")
+    image.show()
+
+getURL()
